8b3683121ffecc31fa3598e76a332e147a3dc844
Atul Bedse <atulbedse@Atuls-MacBook-Air.local>
2026-05-02 15:21:06 +0530

    Reversing the array.

--- deck/src/main.rs
+++ deck/src/main.rs
@@ -1,3 +1,16 @@
+fn reverse<T: Copy>(arr: &mut [T]) {
+    let mut left = 0;
+    let mut right = arr.len()-1;
+    while left < right {
+        let temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        left+=1;
+        right-=1;
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let mut arr = [1,2,3,4,5,6,7,8,9];
+    reverse(&mut arr);
+    println!("Hello, world! {:?}", arr);
 }
